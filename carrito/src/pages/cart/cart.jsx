9a74de59799b457cb6c02f24202843bc629a96f7
jvcalva <jvcalva@tes.edu.ec>
2023-07-18 10:22:13 -0500

    Se agrega el Proyecto

---
+++ carrito/src/pages/cart/cart.jsx
@@ -0,0 +1,46 @@
+import React, { useContext } from "react";
+import { ShopContext } from "../../context/shop-context";
+import { PRODUCTS } from "../../products";
+import { CartItem } from "./cart-item";
+import { useNavigate } from "react-router-dom";
+
+import "./cart.css";
+export const Cart = () => {
+  const { cartItems, getTotalCartAmount, checkout } = useContext(ShopContext);
+  const totalAmount = getTotalCartAmount();
+
+  const navigate = useNavigate();
+
+  return (
+    <div className="cart">
+      <div>
+        <h1>Productos en el Carrito</h1>
+      </div>
+      <div className="cart">
+        {PRODUCTS.map((product) => {
+          if (cartItems[product.id] !== 0) {
+            return <CartItem data={product} />;
+          }
+        })}
+      </div>
+
+      {totalAmount > 0 ? (
+        <div className="checkout">
+          <p> Subtotal: ${totalAmount} </p>
+          <button onClick={() => navigate("/")}> Continuar Comprando </button>
+          <button
+            onClick={() => {
+              checkout();
+              navigate("/checkout");
+            }}
+          >
+            {" "}
+            Pagar{" "}
+          </button>
+        </div>
+      ) : (
+        <h1> Tu carrito de compras esta vacio</h1>
+      )}
+    </div>
+  );
+};
---
+++ carrito/src/products.js
@@ -0,0 +1,59 @@
+import product1 from "./assets/products/1.png";
+import product2 from "./assets/products/2.png";
+import product3 from "./assets/products/3.png";
+import product4 from "./assets/products/4.png";
+import product5 from "./assets/products/5.png";
+import product6 from "./assets/products/6.webp";
+import product7 from "./assets/products/7.webp";
+import product8 from "./assets/products/8.webp";
+
+export const PRODUCTS = [
+  {
+    id: 1,
+    productName: "Mascara",
+    price: 50.0,
+    productImage: product1,
+  },
+  {
+    id: 2,
+    productName: "Cadenas de Oro",
+    price: 250.0,
+    productImage: product2,
+  },
+  {
+    id: 3,
+    productName: "Charango",
+    price: 60.0,
+    productImage: product3,
+  },
+  {
+    id: 4,
+    productName: "Buhos de Madera",
+    price: 25.0,
+    productImage: product4,
+  },
+  {
+    id: 5,
+    productName: "Escultura de Madera",
+    price: 19.99,
+    productImage: product5,
+  },
+  {
+    id: 6,
+    productName: "Silueta Indigena",
+    price: 80.0,
+    productImage: product6,
+  },
+  {
+    id: 7,
+    productName: "Figuritas",
+    price: 5.0,
+    productImage: product7,
+  },
+  {
+    id: 8,
+    productName: "Ollas de barro",
+    price: 40.0,
+    productImage: product8,
+  },
+];
